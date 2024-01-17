569d436c3683bd963acc20c7e16104daa905373f
fajr1mm <fajrimaulana402@gmail.com>
2024-01-17 12:41:57 +0700

    fix host app

--- app.py
+++ app.py
@@ -127,8 +127,8 @@ def update_data_training_csv():
         return jsonify({"error": str(e)}), 500
 #------------------------------------------------------------------------------------------------------------#
 
-# if __name__ == '__main__':
-#     app.run(host='127.0.0.1', port=8080, debug=True)
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=8080, debug=True)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
